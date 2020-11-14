50cc4d617983a5dbe071e3dfd9bd28c16c868273
Marco San Juan <marco@DESKTOP-G605C87.(none)>
2020-11-14 13:46:49 -0800

    Added generateUploadUrl. Removed createImage + getImages

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -5,24 +5,38 @@ import * as uuid from 'uuid'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 const todosTable = process.env.TODOS_TABLE
+const imagesTable = process.env.IMAGES_TABLE
+const bucketName = process.env.IMAGES_S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+
+// For presigned URL
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+})
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Processing event: ', event)
-  const itemId = uuid.v4()
-  const timestamp = new Date().toISOString()
+  
+  // Get Parameter
+  const todoId = event.pathParameters.todoId
+  const validTodoId = await todoExists(todoId)
 
-  const parsedBody = JSON.parse(event.body)
-
-  const newItem = {
-    todoId: itemId,
-    createAt: timestamp,
-    ...parsedBody
+  if (!validTodoId) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        error: 'ToDo does not exist'
+      })
+    }
   }
 
-  await docClient.put({
-    TableName: todosTable,
-    Item: newItem
-  }).promise()
+  const imageId = uuid.v4()
+  const newItem = await createImage(todoId, imageId, event)
+
+  const url = getUploadUrl(imageId)
 
   return {
     statusCode: 201,
@@ -30,7 +44,53 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
-      newItem
+      newItem: newItem,
+      uploadUrl: url
     })
   }
+}
+
+async function todoExists(todoId: string) {
+  const result = await docClient
+    .get({
+      TableName: todosTable,
+      Key: {
+        id: todoId
+      }
+    })
+    .promise()
+
+  console.log('Get todo: ', result)
+  return !!result.Item
+}
+
+async function createImage(todoId: string, imageId: string, event: any) {
+  const timestamp = new Date().toISOString()
+  const newImage = JSON.parse(event.body)
+
+  const newItem = {
+    todoId,
+    timestamp,
+    imageId,
+    ...newImage,
+    imageUrl: `https://${bucketName}.s3.amazonaws.com/${imageId}`
+  }
+  console.log('Storing new item: ', newItem)
+
+  await docClient
+    .put({
+      TableName: imagesTable,
+      Item: newItem
+    })
+    .promise()
+
+  return newItem
+}
+
+function getUploadUrl(imageId: string) {
+  return s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: imageId,
+    Expires: urlExpiration
+  })
 }
