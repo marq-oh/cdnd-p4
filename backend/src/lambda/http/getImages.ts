5943f9cc92e16c1a194c0520c294d394c05d326e
Marco San Juan <marco@DESKTOP-G605C87.(none)>
2020-11-14 12:05:22 -0800

    getImages.ts + images table

---
+++ backend/src/lambda/http/getImages.ts
@@ -0,0 +1,66 @@
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import 'source-map-support/register'
+import * as AWS  from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+const imagesTable = process.env.IMAGES_TABLE
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('Processing event: ', event)
+  
+  // Get Parameter
+  const todoId = event.pathParameters.todoId
+  const validTodoId = await todoExists(todoId)
+
+  if (!validTodoId) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        error: 'ToDo does not exist'
+      })
+    }
+  }
+
+  const images = await getImagesPerTodo(todoId)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items: images
+    })
+  }
+}
+
+async function todoExists(todoId: string) {
+  const result = await docClient
+    .get({
+      TableName: todosTable,
+      Key: {
+        id: todoId
+      }
+    })
+    .promise()
+
+  console.log('Get todo: ', result)
+  return !!result.Item
+}
+
+async function getImagesPerTodo(todoId: string) {
+  const result = await docClient.query({
+    TableName: imagesTable,
+    KeyConditionExpression: 'todoId = :todoId',
+    ExpressionAttributeValues: {
+      ':todoId': todoId
+    },
+    ScanIndexForward: false
+  }).promise()
+
+  return result.Items
+}
