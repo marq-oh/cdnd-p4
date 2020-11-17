a61f0c51379937f86c6bf86316b6eacebc8b4232
Marco San Juan <marco@DESKTOP-G605C87.(none)>
2020-11-16 20:46:59 -0800

    Corrected http validator

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '5d1gye77bi'
+const apiId = 'bo1k3plb47'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
